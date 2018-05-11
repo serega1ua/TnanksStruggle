22ceb4bcc9d83621e7623e80f9574954f9831916
serega1ua <32671836+serega1ua@users.noreply.github.com>
2018-05-11 22:27:34 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import "babel-polyfill";
+ import "babel-polyfill"; 
 
-var regeneratorRuntime = null;
+ 
 
 /* 1. Create class “Fighter” with such properties as name, health and power. 
 These parameters should be set in constructor. */
